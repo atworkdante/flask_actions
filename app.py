6b66c845b00313cefec580d569bc9e06e88165fd
dante-souza <dante.souza@viavarejo.com.br>
2021-06-22 16:05:28 -0300

    Aplicacao

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask
 
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hey its Python Flask application!'
+    name = request.args.get('name', 'World')
+    return f'Hello {name}!'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
