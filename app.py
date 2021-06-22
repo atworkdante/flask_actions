10ab35fff7bc1292ce5c650929861d805fdcaebd
dante-souza <dante.souza@viavarejo.com.br>
2021-06-22 11:01:59 -0300

    adicionando aplicacao

---
+++ app.py
@@ -0,0 +1,14 @@
+import os
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hey its Python Flask application!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.environ.get('listenport', 8080))
